f1dd948ebe2b4d1db63f8e1c0171d6db90462a52
spoutnik16 <jerome@theatreinterface.ch>
2014-09-23 14:44:00 +0200

    db setup and working

--- associations/admin.py
+++ associations/admin.py
@@ -1,14 +1,14 @@
 # Register your models here.
 from django.contrib import admin
 
-from associations.models import Region  # , Association, RegionChild, RegionChild2, Categorie
+from associations.models import Region, Association, RegionChild, RegionChild2, Categorie
 
 
 # Register your models here.
 
 
-# admin.site.register(Association)
+admin.site.register(Association)
 admin.site.register(Region)
-# admin.site.register(RegionChild)
-# admin.site.register(RegionChild2)
-# admin.site.register(Categorie)
+admin.site.register(RegionChild)
+admin.site.register(RegionChild2)
+admin.site.register(Categorie)
