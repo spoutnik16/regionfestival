4e0adc20409bca63d8def3404737ebc10e3fb2a1
spoutnik16 <jerome@theatreinterface.ch>
2014-09-23 13:12:37 +0200

    models into admin

--- association_listing/admin.py
+++ association_listing/admin.py
@@ -1,3 +1,14 @@
+# Register your models here.
 from django.contrib import admin
 
+from association_listing.models import Association, Region, Region_child, Region_child2, Categorie
+
+
 # Register your models here.
+
+
+admin.site.register(Association)
+admin.site.register(Region)
+admin.site.register(Region_child)
+admin.site.register(Region_child2)
+admin.site.register(Categorie)
