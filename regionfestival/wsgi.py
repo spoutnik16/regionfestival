0efd7b657d167acffb5855fa6d34f05c9291ca1a
Jérôme Spoutnik16 Hugon <jerome@iproduction.ch>
2014-11-20 10:01:33 -0500

    wsgimodified

--- regionfestival/wsgi.py
+++ regionfestival/wsgi.py
@@ -6,9 +6,12 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
-
+import sys
 import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/..' )
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "regionfestival.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
