2bc24a1589f5f7fb9f572aeddb2e4312eaeeed34
spoutnik16 <jerome@theatreinterface.ch>
2014-11-20 19:27:55 +0100

    static endly fixed

--- regionfestival/settings.py
+++ regionfestival/settings.py
@@ -110,11 +110,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/web/'
-STATICFILES_DIRS = (
-    'static',
-)
+if not PRODUCTION:
+    STATICFILES_DIRS = ( os.path.join(BASE_DIR, 'static'),)
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
