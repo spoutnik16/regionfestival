5d8640caf2868987e08bd019873f0748fd30b960
Jérôme Spoutnik16 Hugon <jerome@iproduction.ch>
2014-11-22 09:55:48 -0500

    datadump2

---
+++ crm/migrations/0008_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('crm', '0007_auto_20141121_1507'),
+        ('crm', '0007_auto_20141121_1511'),
+    ]
+
+    operations = [
+    ]
