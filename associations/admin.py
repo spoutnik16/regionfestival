5d28c52f96e3d3a6ee4314d5da06c18f421c5ffe
Jérôme Spoutnik16 Hugon <jerome@iproduction.ch>
2014-12-26 11:25:05 -0500

    admin with foreignkey trough

--- associations/admin.py
+++ associations/admin.py
@@ -6,11 +6,21 @@ from associations.models import Region, Association, RegionChild, RegionChild2, 
 
 # Register your models here.
 
+class ContactInline(admin.TabularInline):
+    model = Contact
+    extra = 1
 
-admin.site.register(Association)
+class PersonAdmin(admin.ModelAdmin):
+    inlines = (ContactInline,)
+
+class AssociationAdmin(admin.ModelAdmin):
+    inlines = (ContactInline,)
+
+
+admin.site.register(Association, AssociationAdmin)
 admin.site.register(Region)
 admin.site.register(RegionChild)
 admin.site.register(RegionChild2)
 admin.site.register(CategorieAssociation)
-admin.site.register(Person)
+admin.site.register(Person, PersonAdmin)
 admin.site.register(Contact)
